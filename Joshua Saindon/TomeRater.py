5bd94d5f341ea60568b36e00cad11b9534749970
JoshuaJSaindon <40704945+JoshuaJSaindon@users.noreply.github.com>
2018-09-04 16:31:28 -0700

    Codecademy generated from the project submission page.

---
+++ Joshua Saindon/TomeRater.py
@@ -0,0 +1,93 @@
+class User(object):
+    def __init__(self, name, email):
+        self.name = name
+        self.email = email
+        self.books = books
+    def user_name(name):
+        for user in name:
+            return name
+    def user_email(email):
+        for user in email:
+            return email
+    def books_read(books):
+        books = []
+
+    def get_email(self):
+        def __str__(self):
+        return self.email.as_string()
+
+    def change_email(self, address):
+        pass
+
+    def __repr__(self):
+        return "User" + user_name + "," + " email:" + user_email + "," + "books:" + books_read"
+
+
+    def __eq__(self, other_user):
+        return self.name == other_user
+
+class Book():
+
+    def __init__(self, title,isbn):
+        self.title = title
+        sale.isbn = isbn
+        self.raitings = {}
+
+    def get_title(title):
+        for title in title:
+            return title
+
+    def get_isbn(isbn):
+        for number in isbn:
+            return isbn
+
+    def set_isbn(isbn):
+        for new in isbn:
+            return "This book's ISBN has been updated."
+
+    def add_rating(raiting):
+        rating = self.raitings + raiting
+        if raiting < 0
+        return "Invalid Rating"
+        if rating > 4
+        return "Invalid Rating"
+
+    def __eq__(self,book):
+        return self.book == other_book
+
+class Fiction(Book):
+    def __init__(slef, title, author, isbn):
+        self.author = author
+
+
+class Non_Fiction(Book):
+    def __init__(self, title, subject, level, title, isbn):
+        self.subject = subject
+        self.level = level
+
+
+class TomeRater():
+    def __init__(self):
+        self = []
+
+    def create_book(title, isbn):
+        isbn = 0
+        for number in isbn:
+            return isbn
+
+    def create_novel(title, subject, level, isbn):
+        return author + isbn
+
+    def create_non_fiction(title, subject, level, isbn):
+        return title + isbn + author
+
+    def add_book_to_user(book, email, rating):
+        add_book_to_user = book + email + raiting + user_name
+
+
+    #def print_catalog(self.books):
+    #def print_users(self.books):
+
+
+
+    #I really had no idea what I was doing, still learned some things and thats good, but still... no idea haha.
